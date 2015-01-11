4c35c3831001417b6212ac7d6942648e577926ec
DaHofa <hofer.christoph@liwest.at>
2015-01-11 18:27:05 +0100

    changed to requestAnimationFrame and started a canvas drawing... without success...

--- ProjectX/public_html/Script.js
+++ ProjectX/public_html/Script.js
@@ -11,63 +11,66 @@
 
 function AssetManager() {
 
-    //private variables ad functions
-    var that = this;
+	//private variables ad functions
+	var that = this;
 
-    var downloadQueue = [];
-    var cache = {};
-    var successCount = 0;
-    var errorCount = 0;
+	var downloadQueue = [];
+	var cache = {};
+	var successCount = 0;
+	var errorCount = 0;
 
-    function isDone() {
-        return (downloadQueue.length === successCount + errorCount);
-    }
-
-
-    //public functions
-    this.queueDownload = function (path) {
-        downloadQueue.push(path);
-    };
-
-    //argument is callback function, which calls the remaining code
-    this.downloadAll = function (downloadCallback) {
-        if (downloadQueue.length === 0) {
-            downloadCallback();
-        }
-        for (var i = 0; i < downloadQueue.length; i++) {
-            var path = downloadQueue[i];
-            var img = new Image();
-
-            addHandler(img, "load", function () {
-                successCount += 1;
-                if (isDone()) {
-                    downloadCallback();
-                }
-            });
-
-            addHandler(img, "error", function () {
-                errorCount += 1;
-                if (isDone()) {
-                    downloadCallback();
-                }
-            });
-
-            img.src = path;
-            cache[path] = img;
-        }
-    };
+	function isDone() {
+		return (downloadQueue.length === successCount + errorCount);
+	}
 
 
+	//public functions
+	this.queueDownload = function (path) {
+		downloadQueue.push(path);
+	};
 
-    this.getAsset = function (path) {
-        return cache[path];
-    };
+	//argument is callback function, which calls the remaining code
+	this.downloadAll = function (downloadCallback) {
+		if (downloadQueue.length === 0) {
+			downloadCallback();
+		}
+		for (var i = 0; i < downloadQueue.length; i++) {
+			var path = downloadQueue[i];
+			var img = new Image();
 
-    this.getProgress = function ()
-    {
-        return (1 - (errorCount + successCount) / downloadQueue.length);
-    };
+			addHandler(img, "load", function () {
+				successCount += 1;
+				if (isDone()) {
+					downloadCallback();
+				}
+			});
+
+			addHandler(img, "error", function () {
+				errorCount += 1;
+				if (isDone()) {
+					downloadCallback();
+				}
+			});
+
+			img.src = path;
+			cache[path] = img;
+		}
+	};
+
+
+
+	this.getAsset = function (path) {
+		return cache[path];
+	};
+
+	this.getProgress = function ()
+	{
+		return (1 - (errorCount + successCount) / downloadQueue.length);
+	};
 }
+//----------------------------------------------------------------------------
+
+
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
@@ -75,51 +78,51 @@ function AssetManager() {
 
 function FpsController(fps) {
 
-    //private functions and variables
-    var that = this;
-    var fpsCounter = 0;
-    var loop = '';
+	//private functions and variables
+	var that = this;
+	var fpsCounter = 0;
+	var loop = '';
 
-    function displayFps() {
-        getElem("FPS").innerHTML = "fps: " + fpsCounter + "/" + that.fps;
-        that.resetCount();
-    }
+	function displayFps() {
+		getElem("FPS").innerHTML = "fps: " + fpsCounter + "/" + that.fps;
+		that.resetCount();
+	}
 
-    //public functions and variables
-    this.fps = fps;
+	//public functions and variables
+	this.fps = fps;
 
-    /*
-     * Call this to reset the counter (is done automatically, but is made public
-     * if required
-     * @returns {undefined}
-     */
-    this.resetCount = function () {
-        fpsCounter = 0;
-    };
+	/*
+	 * Call this to reset the counter (is done automatically, but is made public
+	 * if required
+	 * @returns {undefined}
+	 */
+	this.resetCount = function () {
+		fpsCounter = 0;
+	};
 
-    /*
-     * Call this in each frame, increases the counter.
-     * @returns {undefined}
-     */
-    this.countFrame = function () {
-        fpsCounter++;
-    };
+	/*
+	 * Call this in each frame, increases the counter.
+	 * @returns {undefined}
+	 */
+	this.countFrame = function () {
+		fpsCounter++;
+	};
 
-    /*
-     * Call this to start FPS displaying and updating
-     * @returns {undefined}
-     */
-    this.startCount = function () {
-        loop = window.setInterval(displayFps, 1000);
-    };
+	/*
+	 * Call this to start FPS displaying and updating
+	 * @returns {undefined}
+	 */
+	this.startCount = function () {
+		loop = window.setInterval(displayFps, 1000);
+	};
 
-    /*
-     * Call this to stop FPS displaying
-     * @returns {undefined}
-     */
-    this.stopCount = function () {
-        window.clearInterval(loop);
-    };
+	/*
+	 * Call this to stop FPS displaying
+	 * @returns {undefined}
+	 */
+	this.stopCount = function () {
+		window.clearInterval(loop);
+	};
 }
 
 //-----------------------------------------------------------------------------
@@ -135,14 +138,14 @@ function FpsController(fps) {
  */
 addHandler = function (element, handler, func)
 {
-    if (navigator.userAgent.match(/MSIE/))
-    {
-        element.attachEvent('on' + handler, func);
-    }
-    else
-    {
-        element.addEventListener(handler, func, false);
-    }
+	if (navigator.userAgent.match(/MSIE/))
+	{
+		element.attachEvent('on' + handler, func);
+	}
+	else
+	{
+		element.addEventListener(handler, func, false);
+	}
 };
 
 /*
@@ -151,7 +154,7 @@ addHandler = function (element, handler, func)
  * @returns {Element} the element with the id ID
  */
 getElem = function (ID) {
-    return document.getElementById(ID);
+	return document.getElementById(ID);
 };
 
 
@@ -170,84 +173,95 @@ Game = {};
  */
 Game.launch = function ()
 {
-    Game.version = 0.01;
-    Game.fps = 30;
+	Game.version = 0.01;
+	Game.fps = 30;
 
 
-    Game.ready = false;
+	Game.ready = false;
 
-    Game.load = function ()
-    {
-        //Create FPS Controller
-        Game.FpsController = new FpsController(Game.fps);
+	Game.load = function ()
+	{
+		//Create FPS Controller
+		Game.FpsController = new FpsController(Game.fps);
 
-        //Create AssetManager
-        Game.assManager = new AssetManager();
+		//Create AssetManager
+		Game.assManager = new AssetManager();
 
-        //assetManager.queueDownload('img/'); add if pictures are available
-        Game.assManager.downloadAll(Game.init);
-    };
-    Game.init = function () {
-        Game.Account = 0;
-        Game.ready = true;
+		//assetManager.queueDownload('img/'); add if pictures are available
+		Game.assManager.downloadAll(Game.init);
+	};
+	Game.init = function () {
+		Game.Account = 0;
+		Game.ready = true;
 
-        /*
-         * Add Game variables here
-         */
+		/*
+		 * Add Game variables here
+		 */
+		
+		Game.flowerCanvas = getElem("FlowerBackroundCanvas");
+		
 
-        //--------------------------------------------------------------------- 
-        /*
-         * Call functions Here
-         */
-        addHandler(getElem("CoreFlower"), "click", Game.clickFlower);
+		//--------------------------------------------------------------------- 
+		/*
+		 * Call functions Here
+		 */
+		addHandler(getElem("CoreFlower"), "click", Game.clickFlower);
+		addHandler(Game.flowerCanvas,"onmouse",Game.clickBackround);
+		
 
-        Game.FpsController.startCount();
-        setInterval(Game.loop, 1000 / Game.fps);
-    };
+		Game.FpsController.startCount();
+		window.requestAnimationFrame(Game.loop);
+		
+		//setInterval(Game.loop, 1000 / Game.fps);
+	};
 
-    /*
-     * Insert functions here
-     */
+	/*
+	 * Insert functions here
+	 */
+	
+	Game.clickBackround = function(event)
+	{
+		
+	}
+	Game.clickFlower = function (event)
+	{
+		if (event)
+			event.preventDefault();
 
-    Game.clickFlower = function (event)
-    {
-        if (event)
-            event.preventDefault();
-
-        Game.Account++;
-    };
-
-
-    Game.loop = function () {
-
-        /*
-         * Insert Account update
-         */
+		Game.Account++;
+	};
 
 
-        //Draw the frame at the end
-        Game.draw();
-        Game.FpsController.countFrame();
-    };
+	Game.loop = function () {
+		/*
+		 * Insert Account update
+		 */
 
-    Game.draw = function () {
-        //first draw background
-        Game.drawBackground();
 
-        /*
-         * Insert Drawing Account, etc.
-         */
-        getElem("Counter").innerHTML = Game.Account + " X";
-        getElem("Version").innerHTML = "v. " + Game.version;
+		//Draw the frame at the end
+		Game.draw();
+		Game.FpsController.countFrame();
+		window.requestAnimationFrame(Game.loop);
+	};
 
-    };
+	Game.draw = function () {
+		//first draw background
+		Game.drawBackground();
 
-    Game.drawBackground = function () {
+		/*
+		 * Insert Drawing Account, etc.
+		 */
+		getElem("Counter").innerHTML = Game.Account + " X";
+		getElem("Version").innerHTML = "v. " + Game.version;
 
-        /*
-         * redraw the background
-         */
-    };
+	};
+
+	Game.drawBackground = function () {
+
+		/*
+		 * redraw the background
+		 */
+	};
 
 
 };
@@ -264,9 +278,9 @@ Game.launch();
 window.onload = function ()
 {
 
-    if (!Game.ready)
-    {
-        Game.load();
-    }
+	if (!Game.ready)
+	{
+		Game.load();
+	}
 
 };
